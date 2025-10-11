d9fbe94266754fd88adc66ff1485702205317696
Ithairony <ithairony21@gmail.com>
2025-10-11 20:13:13 +0000

    searchTask assignments

--- labTwo_3137299/src/griffith/EvenCounter.java
+++ labTwo_3137299/src/griffith/EvenCounter.java
@@ -1,65 +1,77 @@
 package griffith;
 
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class EvenCounter implements Callable<Integer>{
+public class EvenCounter {
 
 	public static void main(String[] args) {
 
 		int n = 1000000;
-		int nThreads = Runtime.getRuntime().availableProcessors();
+		int nThreads = Runtime.getRuntime().availableProcessors(); // Gets the available number of threads ( Varies according to each machine)  
 		int index[] = new int[nThreads + 1];
-		
-		
-		// Creating a threadpool
-		ExecutorService pool = Executors.newFixedThreadPool(nThreads);
-		System.out.println(pool); // Checking if it was created
 
-		for (int j = 0; j <= nThreads; j++) {
-			index[j] = (j*n) / nThreads;
-		}
-
+		// This loop populates the array with random numbers and according to the index number n  
 		int data[] = new int[n];
-		for (int j = 0; j < data.length; j++) {
-			data[j] = (int) (Math.random()*100000);
+		for (int i = 0; i < data.length; i++) {
+			data[i] = (int) (Math.random()*100000);
 		}
 
-		Thread[] workers	 = new Thread[nThreads];
+		// This loop divides the data[] equally within the available threads
+		for (int i = 0; i <= nThreads; i++) {
+			index[i] = (i*n) / nThreads;
+		}
 
+		// Creating a threadpool according to the number of threads available 
+		ExecutorService pool = Executors.newFixedThreadPool(nThreads);
+		System.out.println("Thread pool created with " + nThreads + " threads.\n" + pool); // Checking if pool was created
 		long startTime = System.currentTimeMillis();
 
-		for (int j = 0; j <nThreads; j++) {
-			workers[j] = new Search(data, index[j], index[j+1]);
-			workers[j].start();
-		}
-
-		for(int j = 0; j < nThreads; j++) {
-			try {
-				workers[j].join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		int freq = 0;
+		// List of futures that will hold the results from each callable
+		ArrayList<Future<int[]>> future = new ArrayList<Future<int[]>>();
 		for (int i = 0; i < nThreads; i++) {
-			freq += ((Search) workers[i]).frequency();
+			Search searchTask = new Search(data, index[i], index[i+1]);
+			future.add(pool.submit(searchTask));
 		}
 
+		System.out.println("Threadpool details " + pool); // Checking if pool was created
+
+		// This bit is being replaced by the callable interface applied already in Search
+
+		// Thread[] workers	 = new Thread[nThreads];
+		//		for (int j = 0; j <nThreads; j++) {
+		//			// Creates individuals threads for each partition 
+		//			workers[j] = new Search(data, index[j], index[j+1]);
+		//			workers[j].start();
+		//		}
+		//
+		//		for(int j = 0; j < nThreads; j++) {
+		//			try {
+		//				workers[j].join();
+		//			} catch (InterruptedException e) {
+		//				e.printStackTrace();
+		//			}
+		//		}
+		//
+		//		int freq = 0;
+		//		for (int i = 0; i < nThreads; i++) {
+		//			freq += ((Search) workers[i]).frequency();
+		//		}
+		
+		// Shuts the pool down so it stops working 
+		pool.shutdown();
+		
 		long endTime =  System.currentTimeMillis();
 		long runningTime = endTime - startTime;
 
-		System.out.println("Even frequency: " + freq);
+		//		System.out.println("Even frequency: " + freq);
+		//		System.out.println("Highest value is : " + highestValue);
 		System.out.println("Running time (ms): " + runningTime);
 		System.out.println("Seconds: " + (runningTime / 1000.0));
 
 	}
-	// Call method implemented by the Callable interface 
-	@Override
-	public Integer call() throws Exception {
-		
-		return null;
-	}
+
 }
--- labTwo_3137299/src/griffith/Search.java
+++ labTwo_3137299/src/griffith/Search.java
@@ -1,30 +1,36 @@
 package griffith;
 
-public class Search extends Thread {
-	
+import java.util.concurrent.Callable;
+
+// In order to use Callable I need to get rid of the Runnable stuff
+public class Search implements Callable<int[]> { // Returns a int [ freq, highestValue ]
+
+	// Declaring variables 
 	int data[];
-	int freq;
 	int lower, upper;
-	int i;
-	
-	// COnstructor 
-	public Search(int d[], int a, int b ) {
-		data = d;
-		lower = a;
-		upper = b;
+
+	// Constructor 
+	public Search(int data[], int lower, int upper ) {
+		this.data = data;
+		this.lower = lower;
+		this.upper = upper;
 	}
+
+	@Override
+	public int[] call() {
 		
-	public void run() {
-		freq = 0;
-		// Loop through the array to find the freq	of even numbers
-		for ( int j = lower; j < upper; j++) {
+		int freq = 0;
+		int highestValue = 0;
+		// Loop through the array to find the frequency and the highestValue 
+		for ( int i = lower; i < upper; i++) {
 			// if x % 2 == 0  means its an even number --> 4%2 == 0  || 5%2 == 1
-			if (data[j] % 2 == 0) 
+			if (data[i] % 2 == 0) 
 				freq++;
+			if (data[i] > highestValue) {
+				highestValue = data[i];
+			}
 		}
+		return new int[] {freq, highestValue};
 	}
-	
-	public int frequency() {
-		return freq;
-	}
+
 }
