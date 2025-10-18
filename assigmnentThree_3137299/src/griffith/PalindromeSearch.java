d3d5722bc02c8a3e6b5ee84c0255d8629df21686
Ithairony <ithairony21@gmail.com>
2025-10-18 17:25:18 +0000

    AssigmnentOne benchmarkPalindromes implementation, next step is to develop searchPalindrome() in the 3 given ways

--- assigmnentThree_3137299/src/griffith/PalindromeSearch.java
+++ assigmnentThree_3137299/src/griffith/PalindromeSearch.java
@@ -10,16 +10,18 @@ package griffith;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PalindromeSearch {
-	
+
 	// Declaring variables 
 	private final static int ROWS = 10;
 	private final static int COLUMNS = 10;
-	
+
 	// Creating a  matrix of Rows X Columns 
 	public static char[][] matrix = new char[ROWS][COLUMNS];
-	
+	private static AtomicInteger count = new AtomicInteger(0);
+
 	// Method to generate the array with random chars 
 	public static char[][] matrixGenerator() {
 		Random random = new Random();
@@ -35,24 +37,52 @@ public class PalindromeSearch {
 	}
 	// Method that prints the array ( Used to check array creation and chars ) 	
 	public static void printMatrix() {
-		 for (int i = 0; i < ROWS; i++) {
-	            for (int j = 0; j < COLUMNS; j++) {
-	                System.out.print(matrix[i][j] + " ");
-	            }
-	            System.out.println();
-	        }
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 	// Method that validates if it's a palindrome 
 	public boolean isPalindrome(String str) {
-		
+
 		int length = str.length();
-		
+
 		for (int i = 0; i < length / 2; i++) {
 			if (str.charAt(i)!= str.charAt(length - i - 1)) {
 				return false;
 			}
 		}
 		return true;
+
+	}
+
+	// Search palindromes in all directions
+	public static void searchPalindromes(int n, int nThreads) {
+
+	}
+	
+	// Method that benchmarks the execution time of the threads with adjustable number of chars per substring to be tested by isPalindrome 
+	public static void benchmarkPalindromes(int minLetters, int maxLetters) {
+		
+		for (int palindromeLength = minLetters; palindromeLength <= maxLetters; palindromeLength++) {
+			System.out.println("Searching for palindromes of length " + palindromeLength);
+
+			int nThreads = Runtime.getRuntime().availableProcessors(); // Gets the available number of threads ( Varies according to each machine)
+
+			for (int t = 1; t <= nThreads; t++) {
+				count.set(0);
+				long startTime = System.nanoTime();
+				searchPalindromes(palindromeLength, t);
+				long endTime = System.nanoTime();
+				double elapsedSeconds = (endTime - startTime) / 1_000_000_000.0;
+
+				System.out.printf("%d palindromes of size %d found in %.6f s using %d thread(s)%n",
+						count.get(), palindromeLength, elapsedSeconds, t);
+			}
+			System.out.println();
+		}
 		
 	}
 	// Getters for rows and Columns 
@@ -62,11 +92,19 @@ public class PalindromeSearch {
 	public int getColumns() {
 		return COLUMNS;
 	} 
-	
+
 	public static void main(String[] args) {
-		
+
 		matrixGenerator();
+		benchmarkPalindromes(3,6);		
 
 		
+
+
+		// Search LEFT - RIGHT --> matrix[i][j + n - 1]
+		//if ( j + n < COLUMNS) {
+
+		// Search UP - DOWN  --> matrix[i + n - 1][j]
+		// Search diagonally UP - DOWN --> matrix[i + n - 1][j + n - 1]
 	}
 }
