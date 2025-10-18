c36e768cd0d8eb2ce2e22b3971a55f7c5d4f190b
Ithairony <ithairony21@gmail.com>
2025-10-18 12:14:01 +0000

    labThree_3137299 creation

---
+++ labThree_3137299/src/griffith/CollectionPoint.java
@@ -0,0 +1,32 @@
+/**
+ * Point.java
+ *
+ * Student Name: Igor Thairony Oliveira Martins
+ * Student Number: 3137299
+ */
+package griffith;
+
+import java.util.*;
+
+class CollectionPoint{
+	List<Point> list = new ArrayList<>();
+
+	public void add(Point p){
+	}
+	
+	public boolean search(Point p){
+		return false;
+	}
+	
+	public List<Point> getAllX(int x){
+		return list;
+	}
+	
+	public void replace(Point p1, Point p2){
+	}
+	
+	public String toString(){
+		return null;
+	}
+}
+
