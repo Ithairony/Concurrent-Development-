5cd9a4f1270b9b15310ee5985adccc5d776f7926
Ithairony <ithairony21@gmail.com>
2025-11-12 19:12:17 +0000

    Assigning a binary Semaphore to Lectuer

--- assigmnentTwo_3137299/src/griffith/Lecturer.java
+++ assigmnentTwo_3137299/src/griffith/Lecturer.java
@@ -3,7 +3,6 @@
  * Student Number: 3137299
  */
 
-
 package griffith;
 
 public class Lecturer extends Thread {
@@ -23,10 +22,10 @@ public class Lecturer extends Thread {
 	public void run() {
 	    try {
 	        while (true) {
-	            enter();
-	            startLecture();
+	            enter();		// Lecturer enter the classroom 
+	            startLecture();	// Lecture starts his class 
 	            Thread.sleep(4000); // simulate lecture time
-	            leave();
+	            leave();		// Lecture leaves the classroom
 	            Thread.sleep(2000); // rest before moving to another classroom
 	        }
 	    } catch (InterruptedException e) {
@@ -35,13 +34,13 @@ public class Lecturer extends Thread {
 	}
 
 	public void enter() throws InterruptedException {
-	    assignedClassroom.lock.lock(); // only one lecturer can enter
+	    assignedClassroom.lecturerSemaphore.acquire(); // Binary semaphore 
 	    assignedClassroom.lecturer = this;
 	    System.out.println(name + " entered " + assignedClassroom.name);
 	}
 
 	public void startLecture() {
-	    assignedClassroom.inSession = true;
+	    assignedClassroom.inSession = true;	// Sets flag that indicates if there is a lecture on to true so that nobody can enter or leave
 	    inSession = true;
 	    System.out.println(name + " started lecture in " + assignedClassroom.name);
 	}
@@ -51,10 +50,9 @@ public class Lecturer extends Thread {
 	    inSession = false;
 	    System.out.println(name + " left " + assignedClassroom.name);
 	    assignedClassroom.lecturer = null;
-	    assignedClassroom.lock.unlock();
+	    assignedClassroom.lecturerSemaphore.release();	// Lecturer leaves -> release access to other lecturers 
 	}
 
-	
 	
 	public void startLecturer() {
 		inSession = true;
